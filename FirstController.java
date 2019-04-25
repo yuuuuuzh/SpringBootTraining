87819d99fa7545b701bafebcb59cb645a2b9b876
yuuzn <43846526+yuuzn@users.noreply.github.com>
2019-04-25 19:34:58 +0900

    Add files via upload

--- FirstController.java
+++ FirstController.java
@@ -1,19 +1,15 @@
 package com.example.demo;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/memo")
 public class FirstController {
 
-	@Autowired
-    private ProductService productService;
+//	@Autowired
+//    private ProductService productService;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String test(Model model) {
@@ -24,15 +20,15 @@ public class FirstController {
 	}
 
 
-	@GetMapping("/new")
-    public List<Product> getAll(){
-        return productService.findAll();
-    }
-
-    @PostMapping("/new")
-    public Product post(Product product){
-        return productService.save(product);
-    }
+//	@GetMapping("/new")
+//    public List<Product> getAll(){
+//        return productService.findAll();
+//    }
+//
+//    @PostMapping("/new")
+//    public Product post(Product product){
+//        return productService.save(product);
+//    }
 
 
 	@GetMapping("new")
